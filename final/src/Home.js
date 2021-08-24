d2018251a4774b217b5c837abe090324c26ab017
rendinel <rendinel@protonmail.com>
2021-08-24 13:01:27 +0200

    push

--- final/src/Home.js
+++ final/src/Home.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { FaBars } from 'react-icons/fa';
-import { useGlobalContext } from './context';
+import React from 'react'
+import { FaBars } from 'react-icons/fa'
+import { useGlobalContext } from './context'
 
 const Home = () => {
   //normal import if we want to import usecontext and appcontext in every component
-// const data = useContext(AppContext)
-//custom hook import
-// const data = useGlobalContext()
-  const { openSidebar, openModal } = useGlobalContext();
+  // const data = useContext(AppContext)
+  //custom hook import
+  // const data = useGlobalContext()
+  const { openSidebar, openModal } = useGlobalContext()
   return (
     <main>
       <button onClick={openSidebar} className='sidebar-toggle'>
@@ -17,7 +17,7 @@ const Home = () => {
         show modal
       </button>
     </main>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
--- final/src/context.js
+++ final/src/context.js
@@ -1,24 +1,24 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext } from 'react'
 
-const AppContext = React.createContext();
+const AppContext = React.createContext()
 //we define like this so we don't need to pass the value we want from one component to another but will be automatically inherited from all the children
 const AppProvider = ({ children }) => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
   const openSidebar = () => {
-    setIsSidebarOpen(true);
-  };
+    setIsSidebarOpen(true)
+  }
   const closeSidebar = () => {
-    setIsSidebarOpen(false);
-  };
+    setIsSidebarOpen(false)
+  }
 
   const openModal = () => {
-    setIsModalOpen(true);
-  };
+    setIsModalOpen(true)
+  }
   const closeModal = () => {
-    setIsModalOpen(false);
-  };
+    setIsModalOpen(false)
+  }
 
   return (
     <AppContext.Provider
@@ -33,14 +33,14 @@ const AppProvider = ({ children }) => {
     >
       {children}
     </AppContext.Provider>
-  );
-};
+  )
+}
 
 // custom hook, when we setup a custom hook we need to start the name with use otherwise we will run into an error
 //because we can't access usecontext in a normal function, we set up a custom hook so we don't need to call in every component usecontext end the
 //context we defineAppcontext in this example but we can simply import the customhook
 export const useGlobalContext = () => {
-  return useContext(AppContext);
-};
+  return useContext(AppContext)
+}
 
-export { AppContext, AppProvider };
+export { AppContext, AppProvider }
